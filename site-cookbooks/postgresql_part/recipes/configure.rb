a74ad7d68c4b310f9cdd97f5aee19483913d3b9c
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2014-10-27 21:54:17 +0900

    Add IPv6 rule and delete unnecessary rule

--- site-cookbooks/postgresql_part/recipes/configure.rb
+++ site-cookbooks/postgresql_part/recipes/configure.rb
@@ -3,9 +3,8 @@ pg_hba = [
   { type: 'local', db: 'all', user: 'postgres', addr: nil, method: 'ident' },
   { type: 'local', db: 'all', user: 'all', addr: nil, method: 'ident' },
   { type: 'host', db: 'all', user: 'postgres', addr: '127.0.0.1/32', method: 'md5' },
+  { type: 'host', db: 'all', user: 'postgres', addr: '::1/128', method: 'md5' },
   { type: 'host', db: 'all', user: 'postgres', addr: '0.0.0.0/0', method: 'reject' },
-  { type: 'host', db: 'all', user: node['postgresql_part']['backup']['user'], addr: '127.0.0.1/32', method: 'md5' },
-  { type: 'host', db: 'all', user: node['postgresql_part']['backup']['user'], addr: '0.0.0.0/0', method: 'reject' }
 ]
 ap_servers = node['cloudconductor']['servers'].select { |_name, server| server['roles'].include?('ap') }
 pg_hba += ap_servers.map do |_name, server|
