52d2519256e3beb9476dc2924370ae4e2d180e9b
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-10-28 02:31:03 +0000

    Add chef-spec for backup default recipe

---
+++ site-cookbooks/backup_restore/spec/recipes/default_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../spec_helper'
+
+describe 'backup_restore::default' do
+  let(:chef_run) do
+    ChefSpec::Runner.new(
+      cookbook_path: %w(site-cookbooks cookbooks),
+      platform:      'centos',
+      version:       '6.5'
+    ).converge(described_recipe)
+  end
+
+  it 'include setup' do
+    expect(chef_run).to include_recipe 'backup_restore::setup'
+  end
+end
