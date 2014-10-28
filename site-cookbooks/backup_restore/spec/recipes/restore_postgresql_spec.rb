101b7fb99bbfda8fb3dfe231ad9b8a46ac9e8651
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-10-28 06:22:03 +0000

    Fix restore process and remove backup user

--- site-cookbooks/backup_restore/spec/recipes/restore_postgresql_spec.rb
+++ site-cookbooks/backup_restore/spec/recipes/restore_postgresql_spec.rb
@@ -6,31 +6,17 @@ describe 'backup_restore::restore_postgresql' do
       cookbook_path: %w(site-cookbooks cookbooks),
       platform:      'centos',
       version:       '6.5'
-    ) do |node|
-      node.set['backup_restore']['sources']['postgresql'] = {
-        db_user: 'root',
-        db_password: '',
-        data_dir: '/etc',
-        version: '9.3.5',
-        run_user: 'mysql',
-        run_group: 'mysql'
-      }
-    end
-    runner.converge(described_recipe)
+    ).converge(described_recipe)
   end
 
   tmp_dir = '/tmp/backup/restore'
   source = {
-    db_user: 'root',
-    db_password: '',
-    data_dir: '/etc',
-    version: '9.3.5',
-    run_user: 'mysql',
-    run_group: 'mysql'
+    port: 5432,
+    user: 'postgres',
+    password: ''
   }
   backup_name = 'postgresql'
   backup_file = "#{tmp_dir}/#{backup_name}.tar"
-  backup_dir = "#{tmp_dir}/#{backup_name}/PostgreSQL.bkpdir"
 
   it 'extract_full_backup' do
     allow(IO::File).to receive(:exist?).and_call_original
@@ -40,35 +26,27 @@ describe 'backup_restore::restore_postgresql' do
     expect(chef_run).to run_bash('extract_full_backup').with(
       code: <<-EOF
     tar -xvf #{backup_file} -C #{tmp_dir}
-    tar -zxvf #{tmp_dir}/#{backup_name}/databases/PostgreSQL.tar.gz -C #{tmp_dir}/#{backup_name}
+    gunzip #{tmp_dir}/#{backup_name}/databases/PostgreSQL.sql.gz
   EOF
     )
   end
 
-  it "postgresql stop" do
-    allow(Dir).to receive(:exist?).and_call_original
-    allow(Dir).to receive(:exist?).with(backup_dir).and_return(true)
-    expect(chef_run).to stop_service("postgresql-#{source[:version]}")
-  end
+  postgresql_connection_info = {
+    host: '127.0.0.1',
+    port: source[:port] || 5432,
+    username: source[:user],
+    password: source[:password]
+  }
 
-  it 'delete_old_data' do
-    allow(Dir).to receive(:exist?).and_call_original
-    allow(Dir).to receive(:exist?).with(backup_dir).and_return(true)
-    expect(chef_run).to run_bash('delete_old_data').with(
-     code: "rm -rf #{source[:data_dir]}/*"
-    )
-  end
-
-  it 'restore_backup' do
-    allow(Dir).to receive(:exist?).and_call_original
-    allow(Dir).to receive(:exist?).with(backup_dir).and_return(true)
-    allow(Dir).to receive(:exist?).with("#{tmp_dir}/#{backup_name}/PostgreSQL.bkpdir").and_return(true)
-    expect(chef_run).to run_bash('restore_backup')
-  end
-
-  it 'postgresql start' do
+  it 'run query for postgresql ' do
     allow(File).to receive(:exist?).and_call_original
-    allow(File).to receive(:exist?).with(backup_file).and_return(true)
-    expect(chef_run).to start_service("postgresql-#{source[:version]}")
+    allow(File).to receive(:exist?).with("#{tmp_dir}/#{backup_name}/databases/PostgreSQL.sql").and_return(true)
+    expect(chef_run).to ChefSpec::Matchers::ResourceMatcher.new(
+      :postgresql_database,
+      :query,
+      'postgres'
+    ).with(
+      connection: postgresql_connection_info
+    )
   end
 end
