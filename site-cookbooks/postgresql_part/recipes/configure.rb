0d27bc769e2975f9432995301827d5526777f1b6
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2014-10-28 10:59:39 +0900

    Fix pg_hba.conf rules

--- site-cookbooks/postgresql_part/recipes/configure.rb
+++ site-cookbooks/postgresql_part/recipes/configure.rb
@@ -2,8 +2,8 @@
 pg_hba = [
   { type: 'local', db: 'all', user: 'postgres', addr: nil, method: 'ident' },
   { type: 'local', db: 'all', user: 'all', addr: nil, method: 'ident' },
-  { type: 'host', db: 'all', user: 'postgres', addr: '127.0.0.1/32', method: 'md5' },
-  { type: 'host', db: 'all', user: 'postgres', addr: '::1/128', method: 'md5' },
+  { type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'md5' },
+  { type: 'host', db: 'all', user: 'all', addr: '::1/128', method: 'md5' },
   { type: 'host', db: 'all', user: 'postgres', addr: '0.0.0.0/0', method: 'reject' }
 ]
 ap_servers = node['cloudconductor']['servers'].select { |_name, server| server['roles'].include?('ap') }
