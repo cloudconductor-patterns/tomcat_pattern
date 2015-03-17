2d27401af65a07ec1719b5975852af3bf32b7869
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-17 08:44:42 +0000

    Remove backup_restore cookbook.

--- site-cookbooks/backup_restore/spec/recipes/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../spec_helper'
-
-describe 'backup_restore::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
-
-  it 'include setup recipe' do
-    expect(chef_run).to include_recipe 'backup_restore::setup'
-  end
-end
