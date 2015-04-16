730b8efafbd466321db2989d4a7331fc61e12838
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-04-16 17:25:56 +0900

    Add the spec of httpd conf to serverspec of test-kitchen in web-configure

--- test/integration/web_configure/serverspec/web_configure_spec.rb
+++ test/integration/web_configure/serverspec/web_configure_spec.rb
@@ -7,3 +7,14 @@ end
 describe port(80) do
   it { should be_listening.with('tcp') }
 end
+
+describe file('/etc/httpd/conf/workers.properties') do
+  it { should be_mode 664 }
+  it { should be_owned_by 'apache' }
+  it { should be_grouped_into 'apache' }
+  it { should contain('worker.loadbalancer.sticky_session=true') }
+  it { should contain('worker.ap_01.reference=worker.template') }
+  it { should contain('worker.ap_01.host=127.0.0.1') }
+  it { should contain('worker.ap_01.route=127.0.0.1') }
+  it { should contain('worker.ap_01.lbfactor=0') }
+end
