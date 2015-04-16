4fe510f63e2ba6aeba8cc0f9d7bf2ba24fdbc47c
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-04-16 15:26:38 +0900

    Add web setup serverspec

---
+++ test/integration/web_setup/serverspec/web_setup_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper.rb'
+
+describe package('httpd') do
+  it { should be_installed }
+end
+
+describe package('httpd-devel') do
+  it { should be_installed }
+end
+
+describe file('/usr/lib64/httpd/modules/mod_jk.so') do
+  it { should be_file }
+end
+
+describe file('/etc/httpd/conf/workers.properties') do
+  it { should be_file }
+end
+
+describe file('/etc/httpd/conf/uriworkermap.properties') do
+  it { should be_file }
+end
+
+describe file('/etc/httpd/conf-available/mod-jk.conf') do
+  it { should be_file }
+  it { should be_mode 664 }
+  it { should be_owned_by 'apache' }
+  it { should be_grouped_into 'apache' }
+end
+
+describe file ('/etc/httpd/conf-enabled/mod-jk.conf') do
+  it { should be_file }
+  it { should be_linked_to '/etc/httpd/conf-available/mod-jk.conf' }
+end
