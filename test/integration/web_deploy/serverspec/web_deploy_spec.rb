2dbcf157efe070c60fc8d783a8b74239ec64788c
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-04-17 16:51:11 +0900

    Add serverspec of test-kitchen in web-deploy

--- test/integration/web_deploy/serverspec/web_deploy_spec.rb
+++ test/integration/web_deploy/serverspec/web_deploy_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
-describe do
+describe service('httpd') do
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening.with('tcp') }
+end
+
+# ToDo Change to attribute value get from Consul
+describe file('/etc/httpd/conf/uriworkermap.properties') do
+  it { should be_file }
+  it { should be_mode 664 }
+  it { should be_owned_by 'apache' }
+  it { should be_grouped_into 'apache' }
+  it { should contain('/jpetstore=loadbalancer') }
+  it { should contain('/jpetstore/*=loadbalancer') }
 end
