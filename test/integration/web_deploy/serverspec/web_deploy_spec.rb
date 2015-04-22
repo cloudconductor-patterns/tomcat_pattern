76a56ca00f44e1478ca70f7c1ea2a796eded08df
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-04-22 17:58:30 +0900

    Modify web_deploy serverspec for test-kitchen as the default-attributes override by the attributes of .kitchen.yml

--- test/integration/web_deploy/serverspec/web_deploy_spec.rb
+++ test/integration/web_deploy/serverspec/web_deploy_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
+require 'json'
+
+kitchen_attributes = open('/tmp/kitchen/dna.json') do |io|
+  JSON.load(io)
+end
+
 chef = ChefSpec::SoloRunner.new
-chef.node.set['cloudconductor']['applications'] = {
-  'application' => {}
-}
+
+chef.node.normal_attrs = kitchen_attributes
+
 chef.converge('role[web_deploy]')
 
 describe service(chef.node['apache']['service_name']) do
@@ -21,6 +27,8 @@ describe file("#{chef.node['apache']['conf_dir']}/uriworkermap.properties") do
   it { should be_mode 664 }
   it { should be_owned_by chef.node['apache']['user'] }
   it { should be_grouped_into chef.node['apache']['group'] }
-  it { should contain('/jpetstore=loadbalancer') }
-  it { should contain('/jpetstore/*=loadbalancer') }
+  chef.node['cloudconductor']['applications'].keys.each do |key|
+    it { should contain("/#{key}=loadbalancer") }
+    it { should contain("/#{key}/*=loadbalancer") }
+  end
 end
