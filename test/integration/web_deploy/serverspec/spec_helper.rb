6905057796ab46c0b5f21b2d171340cfcd4e5d59
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-04-22 15:51:43 +0900

    Modify web_deploy serverspec for test-kitchen use the attributes of chef in serverspec examples

--- test/integration/web_deploy/serverspec/spec_helper.rb
+++ test/integration/web_deploy/serverspec/spec_helper.rb
@@ -1,11 +1,13 @@
 require 'serverspec'
+require 'chefspec'
+require 'ohai'
 
 set :backend, :exec
 
 require 'consul_parameters'
 
-include ConsulParameters
-properties = { consul_parameters: read, servers: read_servers }
+
+pattern_root_dir = '/tmp/kitchen'
 
 RSpec.configure do |c|
   if ENV['ASK_SUDO_PASSWORD']
@@ -15,5 +17,13 @@ RSpec.configure do |c|
     c.sudo_password = ENV['SUDO_PASSWORD']
   end
 
-  set_property properties
+
+  c.cookbook_path = File.join(pattern_root_dir, 'cookbooks')
+  c.role_path = File.join(pattern_root_dir, 'roles')
+
+  ohai = Ohai::System.new
+  ohai.all_plugins('platform')
+
+  c.platform = ohai[:platform]
+  c.version = ohai[:platform_version]
 end
--- test/integration/web_deploy/serverspec/web_deploy_spec.rb
+++ test/integration/web_deploy/serverspec/web_deploy_spec.rb
@@ -1,19 +1,26 @@
 require 'spec_helper'
 
-describe service('httpd') do
+chef = ChefSpec::SoloRunner.new
+chef.node.set['cloudconductor']['applications'] = {
+  'application' => {}
+}
+chef.converge('role[web_deploy]')
+
+describe service(chef.node['apache']['service_name']) do
   it { should be_running }
 end
 
-describe port(80) do
-  it { should be_listening.with('tcp') }
+chef.node['apache']['listen_ports'].each do |listen_port|
+  describe port(listen_port) do
+    it { should be_listening.with('tcp') }
+  end
 end
 
-# ToDo Change to attribute value get from Consul
-describe file('/etc/httpd/conf/uriworkermap.properties') do
+describe file("#{chef.node['apache']['conf_dir']}/uriworkermap.properties") do
   it { should be_file }
   it { should be_mode 664 }
-  it { should be_owned_by 'apache' }
-  it { should be_grouped_into 'apache' }
+  it { should be_owned_by chef.node['apache']['user'] }
+  it { should be_grouped_into chef.node['apache']['group'] }
   it { should contain('/jpetstore=loadbalancer') }
   it { should contain('/jpetstore/*=loadbalancer') }
 end
