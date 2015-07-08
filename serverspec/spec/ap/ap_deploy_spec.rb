ce2e9bff04679abaf845d11af556c631e19dac8c
konda <konda.tomoyuki@tis.co.jp>
2015-07-08 10:39:07 +0900

    Wait for tomcat auto deploy

--- serverspec/spec/ap/ap_deploy_spec.rb
+++ serverspec/spec/ap/ap_deploy_spec.rb
@@ -7,6 +7,8 @@ describe 'application http status' do
   apps.each do |app_name, app|
     next if app[:type] == 'optional'
 
+    sleep 3
+
     describe "#{app_name}" do
       describe command("curl -s --noproxy localhost 'http://localhost:8080/#{app_name}' -w \"%{http_code}\" -o /dev/null") do
         its(:stdout) { should_not match '000' }
