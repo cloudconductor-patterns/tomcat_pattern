c4a17d7389fddb438464b7551d3d95729cd310c5
Yo Takezawa <takezawa.yo@tis.co.jp>
2016-03-22 13:56:42 +0900

    Use be_reachable matcher instead of hping3 to avoid bug in specinfra

--- serverspec/spec/web/web_configure_spec.rb
+++ serverspec/spec/web/web_configure_spec.rb
@@ -18,8 +18,8 @@ describe 'connect ap_servers' do
   end
 
   ap_servers.each do |server|
-    describe command("hping3 -S #{server[:private_ip]} -p 8009 -c 5") do
-      its(:stdout) { should match(/sport=8009 flags=SA/) }
+    describe host(server[:private_ip]) do
+      it { should be_reachable.with(port: 8009) }
     end
   end
 end
